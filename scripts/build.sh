00e281c739e1ac5afe54e929e0d9184fe225d582
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2021-10-04 16:03:22 -0400

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,3 +7,5 @@ bin/pulsar-admin topics create persistent://public/default/energy
 bin/pulsar-admin topics list public/default
 
 bin/pulsar-client consume "persistent://public/default/energy" -s mobile1
+
+python -m SimpleHTTPServer 8000
