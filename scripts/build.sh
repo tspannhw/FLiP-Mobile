7e41ff155594b2576e3252c6af3599d22dfba0aa
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2021-10-04 12:21:48 -0400

    Create build.sh

---
+++ scripts/build.sh
@@ -0,0 +1,6 @@
+bin/pulsar-admin topics create persistent://public/default/transcom
+
+bin/pulsar-admin topics create persistent://public/default/weather
+
+bin/pulsar-admin topics create persistent://public/default/energy
+
